2a97072d5cd5b01d41575875a00e13a0361e9ab1
root <root@root.root>
2014-09-16 19:13:39 -0430

    Actualización del capitulo #29 en donde nuestra rest ya está al 95% de completada

---
+++ Sails/restBlog/api/controllers/AuthController.js
@@ -0,0 +1,43 @@
+/**
+ * AuthController
+ *
+ * @description :: Server-side logic for managing auths
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+var bcrypt = require('bcrypt');
+
+module.exports = {
+	
+	login: function(req, res){
+		User.findOne({ username: req.param('username') }, function(err, user){
+			if(err){
+				console.log(err);
+			}
+			if(user){
+				bcrypt.compare(req.param('password'), user.password, function(err, match) {
+				    if(err){
+				    	console.log(err);
+				    }
+				    if(match){
+				    	req.session.user = user;
+				    	req.session.authenticated = true;
+				    	res.send({
+				    		user: user
+				    	});
+				    	return console.log(user);
+				    } else{
+				    	return console.log('La contraseña o el usuario no existen');
+				    }
+				});
+			} else{
+				return console.log('La contraseña o el usuario no existen');
+			}
+		})
+	},
+
+	logout: function(req, res){
+		req.session.destroy();
+		return res.json(200);
+	}
+};
+
